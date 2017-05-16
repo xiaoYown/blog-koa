c7efdba07e3a1efc048966f38703472b1e4a7774
DESKTOP-9MT21FL\xiaoYown <qzy09101018@sina.com>
2017-05-16 23:37:47 +0800

    Mod_router splite

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,9 @@ const isLogin = require('../utils/login').isLogin;
 
 router.get('/', isLogin , function *( next ) {
 		yield this.render('admin', {layout: false, title: '用户管理'});
+	})
+	.get('/blog', isLogin , function *( next ) {
+		yield this.render('admin-blog', {layout: false, title: '用户管理-blog'});
 	});
 
 module.exports = router;
