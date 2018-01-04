9506f5c452a6680adf14eca30f2ffb8765d03b5a
xiaoYown <511687372@qq.com>
2018-01-04 19:54:00 +0800

    no message

--- routes/blog.js
+++ routes/blog.js
@@ -2,22 +2,10 @@ const router = require('koa-router')();
 const db_operate = require('../mysql').db_operate;
 
 router.get('/', function *( next ){
-  let articals = yield db_operate.query(
-		`SELECT 
-		title,
-		id,
-		type,
-		top,
-		description,
-		DATE_FORMAT(create_time,'%Y.%m.%d %H:%i') AS create_time, 
-		DATE_FORMAT(update_time,'%Y.%m.%d %H:%i') AS update_time
-		FROM articals WHERE type = "blog" order by top desc`
-	);
-	yield this.render('blog', {
-		layout: false,
-		title: '首页',
-		articals
-	});
+  yield this.render('index', {
+    layout: false,
+    title: 'xiaoYown'
+  });
 })
 .get('/home', function *( next ) {
 	let articals = yield db_operate.query(
