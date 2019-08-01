5dc3d34197efdf6a2e76e290b9f97b03b20b0624
xiaoYown <qzy09101018@gmail.com>
2019-08-01 23:09:19 +0800

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -4,9 +4,9 @@ var wrapper = require('co-mysql'),
 var options = {
 	host : 'localhost',
 	port : 3306 ,
-	database : 'blog_koa',
+	database : 'blog',
 	user: 'root',
-	password : '511687372'
+	password : 'Mysql_511687372'
 };
 
 var pool = mysql.createPool(options);
